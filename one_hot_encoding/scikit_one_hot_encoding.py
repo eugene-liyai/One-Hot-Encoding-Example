0dafb82d0be0139b2fa1d558c414dda904fb5edd
Andela-eugene <eugene.liyai@andela.com>
2018-01-06 00:38:52 +0300

    [Feat] add data input

--- one_hot_encoding/scikit_one_hot_encoding.py
+++ one_hot_encoding/scikit_one_hot_encoding.py
@@ -2,3 +2,8 @@
 from numpy import array, argmax
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+
+# define input data
+data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
+values = array(data)
+print(values)
