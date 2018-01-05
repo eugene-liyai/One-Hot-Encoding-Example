5d015541c07546e0ebf03bf9d001392e9d7ba1b1
Andela-eugene <eugene.liyai@andela.com>
2018-01-06 00:42:53 +0300

    [Feat] add integer encoding

--- one_hot_encoding/scikit_one_hot_encoding.py
+++ one_hot_encoding/scikit_one_hot_encoding.py
@@ -7,3 +7,10 @@ from sklearn.preprocessing import OneHotEncoder
 data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
 values = array(data)
 print(values)
+
+# integer encode
+label_encoding = LabelEncoder()
+integer_encoded = label_encoding.fit_transform(values)
+print(integer_encoded)
+
+# binary encode
