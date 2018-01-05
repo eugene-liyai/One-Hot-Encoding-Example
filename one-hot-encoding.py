bf7ed23e2a182c33aa75c11a4780dbc58b44192b
Andela-eugene <eugene.liyai@andela.com>
2018-01-05 12:01:03 +0300

    [Feat] add hot encoding

--- one-hot-encoding.py
+++ one-hot-encoding.py
@@ -15,3 +15,13 @@ int_to_char = dict((i, c) for i, c in enumerate(alphabet))
 # integer encode input data
 integer_encode = [char_to_int[char] for char in data]
 print(integer_encode)
+
+# one hot encode
+onehot_encode = list()
+for value in integer_encode:
+    letter = [0 for _ in range(len(alphabet))]
+    letter[value] = 1
+    onehot_encode.append(letter)
+print(onehot_encode)
+
+# invert encoding
