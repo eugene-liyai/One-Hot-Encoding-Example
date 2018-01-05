776e70b4025a997a460d7447d33c04a62259cd0f
Andela-eugene <eugene.liyai@andela.com>
2018-01-05 11:54:42 +0300

    [Feat] integer encode input data

--- one-hot-encoding.py
+++ one-hot-encoding.py
@@ -4,3 +4,14 @@ from numpy import argmax
 # define input
 data = 'hello world'
 print(data)
+
+# define universe of possible input values
+alphabet = 'abcdefghijklmnopqrstuvwxyz '
+
+# define a mapping of chars of integers
+char_to_int = dict((c, i) for i, c in enumerate(alphabet))
+int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+
+# integer encode input data
+integer_encode = [char_to_int[char] for char in data]
+print(integer_encode)
