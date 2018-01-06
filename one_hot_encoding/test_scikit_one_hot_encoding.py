f68716255a2a8b0d74bb59a8e369f9208a0f2c07
Andela-eugene <eugene.liyai@andela.com>
2018-01-06 12:22:10 +0300

    [Feat] test input data and value

--- one_hot_encoding/test_scikit_one_hot_encoding.py
+++ one_hot_encoding/test_scikit_one_hot_encoding.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
+import numpy
 from .scikit_one_hot_encoding import *
 
 
 class Scikit_One_Hot_Encoding(TestCase):
     '''Scikit One hot encoding tests'''
 
-    def test_input_string(self):
-        self.assertEqual(True, isinstance(data, str))
+    def test_input_data(self):
+        self.assertEqual(True, isinstance(data, list))
+
+    def test_value_input(self):
+        self.assertEqual(numpy.ndarray, type(values))
